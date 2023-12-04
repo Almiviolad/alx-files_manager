2320149e3ba1ff6e826401a7ff0089b14810ae7b
Aloh Michael <mykelalohgmail.com>
2023-12-03 22:46:20 -0800

    task 2

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const AppController = require('../controllers/AppController');
+router.get('/status', (req, res) => {
+    const result = AppController.getStatus();
+    if (result.redis && result.db) {
+        res.status(200).json(result);
+    } else {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+router.get('/stats', async (req, res) => {
+    try {
+        const result = await getStats();
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+module.exports = router;
